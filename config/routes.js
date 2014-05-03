e994b5eba5fd3d134e53a933424f370bab021564
Muhammad Reda <say.hello@mreda.net>
2014-05-03 13:42:22 +0200

    Added route /blog to list all posts.

--- config/routes.js
+++ config/routes.js
@@ -2,13 +2,21 @@ module.exports = function(app, router) {
 
     app.use(router);
     app.use(function(req, res) {
-        res.status(404).render('404', { title: 'Page not found (AKA 404 Error)' });
+        res.status(404).render('404', {
+            title: 'Page not found (AKA 404 Error)'
+        });
     });
 
     //home route
     var home = require('../app/controllers/home_ctrl');
     router.get('/', function(req, res, next) {
         home.index(req, res, next);
+    });
+
+    // post route
+    var post = require('../app/controllers/post_ctrl');
+    router.get('/blog', function(req, res, next) {
+        post.index(req, res, next);
     });
 
 };

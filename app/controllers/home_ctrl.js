54bddd06511675ef4d27fa52b8f3d63f703d1341
Muhammad Reda <say.hello@mreda.net>
2014-05-03 13:45:47 +0200

    Updated homepage view

--- app/controllers/home_ctrl.js
+++ app/controllers/home_ctrl.js
@@ -2,7 +2,7 @@ exports.index = function(req, res, next) {
 
     res.render('home/index', {
         title: 'Welcome to Node-Express-Mongo MVC',
-        body: ''
+        body: 'MVC application structure for NodeJs using Express 4.1+ and MongoDB'
     });
 
 };
